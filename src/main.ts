86c3696d3aed9d2f49896c6b96c026c93702412e
QuanKhs <quanphamluong@gmail.com>
2021-06-24 15:22:42 +0700

    define swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { setupSwagger } from './swagger/index';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
---
+++ src/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = 'api/docs';
+export const SWAGGER_API_NAME = 'urTicket API';
+export const SWAGGER_API_DESCRIPTION = 'urTicket API Description';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
