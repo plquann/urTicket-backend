6a66c9bea752bd035974e9d8873f7d45c4ffb5ed
QuanKhs <quanphamluong@gmail.com>
2021-09-01 10:00:33 +0700

    validate time showtime period

--- src/showtimes/showtimes.service.ts
+++ src/showtimes/showtimes.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MovieService } from 'src/movies/movie.service';
 import { TheatersService } from 'src/theaters/theaters.service';
 import { TicketsService } from 'src/tickets/tickets.service';
-import { Repository } from 'typeorm';
+import { Raw, Repository } from 'typeorm';
 import { CreateShowtimeDto } from './dto/create-showtime.dto';
 import { UpdateShowtimeDto } from './dto/update-showtime.dto';
 import { Showtime } from './entities/showtime.entity';
@@ -32,9 +32,7 @@ export class ShowtimesService {
     return showtimes;
   }
 
-  async createShowtime(
-    createShowtimeDto: CreateShowtimeDto,
-  ): Promise<Showtime> {
+  async createShowtime(createShowtimeDto: CreateShowtimeDto): Promise<any> {
     const { movieId, theaterId, startTime, room } = createShowtimeDto;
 
     const movie = await this.movieService.getMovieById(movieId);
@@ -54,22 +52,35 @@ export class ShowtimesService {
       );
     }
 
-    //check time
-    
-    // const showtimes = await this.showtimeRepository
-    //   .createQueryBuilder()
-    //   .where('theaterId = :theaterId', { theaterId: theater.id })
-    //   .andWhere('startTime >= :startTime', {
-    //     startTime: createShowtimeDto.startTime,
-    //   })
-    //   .getMany();
-
-    // const showtime = this.showtimeRepository.create(createShowtimeDto);
-    // return await this.showtimeRepository.save(showtime);
     const start = new Date(startTime);
     const endTime = new Date(start.getTime() + movie.duration * 60000);
 
+    const showtimes = await this.showtimeRepository.find({
+      where: [
+        {
+          theaterId,
+          room,
+          startTime: Raw((alias) => `${alias} <= :date`, { date: start }),
+          endTime: Raw((alias) => `${alias} >= :date`, { date: start }),
+        },
+        {
+          theaterId,
+          room,
+          startTime: Raw((alias) => `${alias} <= :date`, { date: endTime }),
+          endTime: Raw((alias) => `${alias} >= :date`, { date: endTime }),
+        },
+      ],
+    });
+
+    if (showtimes.length > 0) {
+      throw new HttpException(
+        `Already have showtime in this time period!`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     //create Show time
+    
     const result = await this.showtimeRepository
       .createQueryBuilder()
       .insert()
@@ -78,7 +89,10 @@ export class ShowtimesService {
       .execute();
 
     // create tickets
-    const seats = await this.theaterService.getSeatsByTheaterIdAndRoom(theaterId, room);
+    const seats = await this.theaterService.getSeatsByTheaterIdAndRoom(
+      theaterId,
+      room,
+    );
 
     await this.ticketService.createTickets(seats, result.identifiers[0].id);
     // console.log(
