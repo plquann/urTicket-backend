052e88dcc4c52232f39f59cc5547d7248a5583b3
QuanKhs <quanphamluong@gmail.com>
2021-09-04 22:54:25 +0700

    query transaction

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import StripeService from 'src/stripe/stripe.service';
 import { TicketsService } from 'src/tickets/tickets.service';
@@ -33,5 +33,23 @@ export class ReservationsService {
 
     const user = await this.usersService.getUserById(userId);
     //charge => create reservation => update ticket => send email
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // execute some operations on this transaction
+
+      // commit transaction now:
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      console.log('🚀 ~ file: reservations.service.ts ~ line 46 ~ e', e);
+      // since we have errors let's rollback changes we made
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException();
+    } finally {
+      // you need to release query runner which is manually created:
+      await queryRunner.release();
+    }
   }
 }
