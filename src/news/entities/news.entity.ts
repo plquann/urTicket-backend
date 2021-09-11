eec6040409a348e082b2c1354c140d26321a7202
QuanKhs <quanphamluong@gmail.com>
2021-09-11 20:53:21 +0700

    news entity

--- src/news/entities/news.entity.ts
+++ src/news/entities/news.entity.ts
@@ -1,1 +1,32 @@
-export class News {}
+import { BaseEntity } from 'src/base/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity('News')
+export class News extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  isPublished: boolean;
+
+  @Column()
+  publishedDate: Date;
+
+  @Column('simple-array')
+  category: string[];
+
+  @Column()
+  views: number;
+
+  @Column('simple-json')
+  author: { name: string; introduction: string };
+}
