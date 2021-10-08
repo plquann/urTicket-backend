f5b9074ef29c4926645c588746ef019f30b66bbc
codefactor-io <support@codefactor.io>
2021-10-08 07:51:08 +0000

    [CodeFactor] Apply fixes

--- src/theaters/theaters.controller.ts
+++ src/theaters/theaters.controller.ts
@@ -10,7 +10,7 @@ export class TheatersController {
   @Get()
   getAllTheaters() {
     return this.theatersService.getAllTheaters();
-  } 
+  }
 
   @Get(':id')
   getTheaterById(@Param('id') id: string) {
